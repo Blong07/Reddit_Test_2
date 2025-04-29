ff597ffc6923445396884b690a528e40d557be14
Alex Blong <alexanderblong07@gmail.com>
2025-04-29 19:12:09 +1200

    Initial Commit

---
+++ Reddit_Test_2Tests/Reddit_Test_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Reddit_Test_2Tests.swift
+//  Reddit_Test_2Tests
+//
+//  Created by Alexander Blong on 29/04/2025.
+//
+
+import Testing
+@testable import Reddit_Test_2
+
+struct Reddit_Test_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
